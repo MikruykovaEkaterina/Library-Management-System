a8055af9dc8fdf53c08c95147b8e3f98398e31bd
MikruykovaEkaterina <olakudrina687@gmail.com>
2025-06-18 18:48:24 +0400

    изменила ToString()
    Теперь Id тоже выводится

---
+++ Library/Library/Book.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+  public class Book
+  {
+    static private int _ID = 1;
+    private string title; //название
+    public string Title
+    {
+      get { return title; }
+      set
+      {
+        // Сюда проверки на корректность нового значения
+        title = value;
+      }
+    }
+
+    private string author; //автор
+    public string Author
+    {
+      get { return author; }
+      set
+      {
+        // Сюда проверки на корректность нового значения
+        author = value;
+      }
+    }
+
+    private int year; //год
+    public int Year
+    {
+      get { return year; }
+      set
+      {
+        // Сюда проверки на корректность нового значения
+        year = value;
+      }
+    }
+
+    private string genre; //жанр
+    public string Genre
+    {
+      get { return genre; }
+      set
+      {
+        // Сюда проверки на корректность нового значения
+        genre = value;
+      }
+    }
+    public bool IsAvailable { get; set; } //статус
+    public int ID { get; private set; } //id книги
+    
+    //Хранение информации в виде строки
+    public Book(string title, string author, int year, string genre)
+    {
+      this.ID = _ID;
+		  _ID++;
+      Title = title;
+      Author = author;
+      Year = year;
+      Genre = genre;
+      IsAvailable = true;
+    }
+
+    public override string ToString()
+    {
+      return $"Id: {ID}, название: '{Title}', автор: '{Author}', год издания: {Year}, жанр: '{Genre}' - статус: {(IsAvailable ? "доступна" : "взята")}";
+    }
+  }
+}
